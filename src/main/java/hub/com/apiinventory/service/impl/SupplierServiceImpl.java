f55a07f64ec536bf2b1bbb8e74fc22e5db12199c
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-23 08:29:17 -0500

    feat(supplier): SupplierServiceImpl implement GET getById

---
+++ src/main/java/hub/com/apiinventory/service/impl/SupplierServiceImpl.java
@@ -0,0 +1,27 @@
+package hub.com.apiinventory.service.impl;
+
+import hub.com.apiinventory.dto.SupplierDTOResponse;
+import hub.com.apiinventory.entity.Supplier;
+import hub.com.apiinventory.mapper.SupplierMapper;
+import hub.com.apiinventory.repo.SupplierRepository;
+import hub.com.apiinventory.service.SupplierService;
+import hub.com.apiinventory.service.domain.SupplierServiceDomain;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SupplierServiceImpl implements SupplierService {
+
+    private final SupplierServiceDomain supplierServiceDomain;
+    private final SupplierMapper supplierMapper;
+
+    @Override
+    public Mono<SupplierDTOResponse> getById(Long id) {
+        return supplierServiceDomain.findByIdOrError(id)
+                .map(supplierMapper::toResponse);
+    }
+}
