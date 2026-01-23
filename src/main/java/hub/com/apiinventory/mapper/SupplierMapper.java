73c0143522b25182975c111d6db355e6e81bbe6c
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-23 08:06:03 -0500

    feat(supplier): SupplierMapper add mapped toResponse(Entity)

---
+++ src/main/java/hub/com/apiinventory/mapper/SupplierMapper.java
@@ -0,0 +1,12 @@
+package hub.com.apiinventory.mapper;
+
+import hub.com.apiinventory.dto.SupplierDTOResponse;
+import hub.com.apiinventory.entity.Supplier;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SupplierMapper {
+    // to response
+    SupplierDTOResponse toResponse(Supplier supplier);
+
+}
