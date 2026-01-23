8f78bd70e8c4eaed0bc27fce9ed83731cbaec134
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-23 08:50:11 -0500

    chore(exception): add BussinessException , ErrorResponse,ResourceNotFoundException implement to GlobalExceptionHandler

---
+++ src/main/java/hub/com/apiinventory/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package hub.com.apiinventory.exception;
+
+public class ResourceNotFoundException extends BusinessException {
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
