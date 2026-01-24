d98869995997cd46481ba20b560af0cac8ea54ae
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-23 20:47:43 -0500

    test(supplier): SupplierServiceDomainTest add methods findByIdOrError(Success and Fail)

---
+++ src/test/java/hub/com/apiinventory/service/domain/SupplierServiceDomainTest.java
@@ -0,0 +1,70 @@
+package hub.com.apiinventory.service.domain;
+
+import hub.com.apiinventory.entity.Supplier;
+import hub.com.apiinventory.exception.ResourceNotFoundException;
+import hub.com.apiinventory.nums.ExceptionMessages;
+import hub.com.apiinventory.repo.SupplierRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SupplierServiceDomainTest {
+
+    @Mock
+    private SupplierRepository supplierRepository;
+
+    @InjectMocks
+    private SupplierServiceDomain supplierServiceDomain;
+
+    @Nested
+    @DisplayName("Test findByIdOrError")
+    class findByIdOrErrorTest{
+        @Test
+        @DisplayName("Test findByIdOrError Success")
+        void findByIdOrErrorSuccess(){
+            // Arrange
+            Supplier supplier = new Supplier(1L,"name Test","email_test@gmail.com","+51 9827364352");
+            Long idExist = 1L;
+
+            when(supplierRepository.findById(idExist)).thenReturn(Mono.just(supplier));
+            // Act & Assert
+            StepVerifier.create(supplierServiceDomain.findByIdOrError(idExist))
+                    .expectNextMatches(s ->
+                            s.getId().equals(idExist)
+                            && s.getName().equals("name Test")
+                            && s.getEmail().equals("email_test@gmail.com")
+                            && s.getPhone().equals("+51 9827364352")
+                    )
+                    .verifyComplete();
+
+            // Verify
+            verify(supplierRepository,times(1)).findById(idExist);
+        }
+
+        @Test
+        @DisplayName("Test findByIdOrError Fail")
+        void findByIdOrErrorFail(){
+            // Arrange
+            when(supplierRepository.findById(1L)).thenReturn(Mono.empty());
+
+            // Act & Assert
+            StepVerifier.create(supplierServiceDomain.findByIdOrError(1L))
+                    .expectErrorMatches(throwable ->
+                            throwable instanceof ResourceNotFoundException &&
+                                    throwable.getMessage().equals(ExceptionMessages.RESOURCE_NOT_FOUND_ERROR.message() + "1")
+                    )
+                    .verify();
+
+            verify(supplierRepository, times(1)).findById(1L);
+        }
+    }
+}
