6d50a85b008d8191c8b2b772040401203401e69e
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-25 00:22:46 -0500

    feat(supplier): SupplierService add GET findAllPage

--- src/main/java/hub/com/apiinventory/service/SupplierService.java
+++ src/main/java/hub/com/apiinventory/service/SupplierService.java
@@ -1,10 +1,13 @@
 package hub.com.apiinventory.service;
 
 import hub.com.apiinventory.dto.SupplierDTOResponse;
+import hub.com.apiinventory.util.page.PageResponse;
 import reactor.core.publisher.Mono;
 
 public interface SupplierService {
     // GET
     Mono<SupplierDTOResponse> getById(Long id);
+    Mono<PageResponse<SupplierDTOResponse>> findAllPage(int page, int size);
+
 
 }
