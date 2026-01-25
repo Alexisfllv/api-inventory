3debafdb2fa6ba84dcb620ca980fe3c5e766d7f4
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-25 00:24:15 -0500

    feat(supplier)SupllierServiceImpl add implement GET findAllPage

--- src/main/java/hub/com/apiinventory/service/impl/SupplierServiceImpl.java
+++ src/main/java/hub/com/apiinventory/service/impl/SupplierServiceImpl.java
@@ -2,12 +2,18 @@ package hub.com.apiinventory.service.impl;
 
 import hub.com.apiinventory.dto.SupplierDTOResponse;
 import hub.com.apiinventory.mapper.SupplierMapper;
+import hub.com.apiinventory.repo.SupplierRepository;
 import hub.com.apiinventory.service.SupplierService;
 import hub.com.apiinventory.service.domain.SupplierServiceDomain;
+import hub.com.apiinventory.util.page.PageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +23,38 @@ public class SupplierServiceImpl implements SupplierService {
     private final SupplierServiceDomain supplierServiceDomain;
     private final SupplierMapper supplierMapper;
 
+    // uso directo no reutilizable
+    private final SupplierRepository supplierRepository;
+
     @Override
     public Mono<SupplierDTOResponse> getById(Long id) {
         return supplierServiceDomain.findByIdOrError(id)
                 .map(supplierMapper::toResponse);
+    }
+
+    public Mono<PageResponse<SupplierDTOResponse>> findAllPage(int page, int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        Mono<List<SupplierDTOResponse>> contentMono =
+                supplierRepository.findAllBy(pageable)
+                        .map(supplierMapper::toResponse)
+                        .collectList();
+
+        Mono<Long> totalElementsMono = supplierRepository.count();
+
+        return Mono.zip(contentMono, totalElementsMono)
+                .map(tuple -> {
+                    long totalElements = tuple.getT2();
+                    int totalPages = (int) Math.ceil((double) totalElements / size);
+
+                    return new PageResponse<>(
+                            tuple.getT1(),
+                            page,
+                            size,
+                            totalElements,
+                            totalPages
+                    );
+                });
     }
 }
