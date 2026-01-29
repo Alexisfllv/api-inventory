2dbdb9f22b75b7c1e5fc02c763c124bf74331e87
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-28 19:29:35 -0500

    chore(auditory): create Auditing and add Supplier   (#7)
    * chore(auditing): auditiong add R2dbcAuditingConfig and BaseEntityReactive
    
    * feat(supplier) Supplier add extend BaseEntityReactive created updated

---
+++ src/main/java/hub/com/apiinventory/util/auditing/R2dbcAuditingConfig.java
@@ -0,0 +1,9 @@
+package hub.com.apiinventory.util.auditing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+
+@Configuration
+@EnableR2dbcAuditing
+public class R2dbcAuditingConfig {
+}
